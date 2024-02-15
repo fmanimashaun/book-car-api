0ba53e8a76d54901ecefeefdf4f2ad7500a85c9c
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-15 16:02:29 +0100

    Reset the rspec unit test codes

--- spec/routing/api/v1/cities_routing_spec.rb
+++ spec/routing/api/v1/cities_routing_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::CitiesController, type: :routing do
+RSpec.describe API::V1::CitiesController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
       expect(get: '/api/v1/cities').to route_to('api/v1/cities#index')
