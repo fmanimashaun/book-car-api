9a5cd62881c2cd9dc7a7573159cbd35039b168e3
Esteban <cepalacios@outlook.com>
2024-02-15 18:00:51 -0500

    Add tests for cities_rspec
    Modified car factory so it has a longer description as the validation
    requires a min length fo 10 characters

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:city) { FactoryBot.create(:city) }
+
+  it "Creates a city with valid attributes" do
+    expect(city).to be_valid
+  end
+
+  describe "fails to create a city with invalid attributes" do
+    it "Name attribute is not present" do
+      city = FactoryBot.build(:city, name: nil)
+
+      expect(city).to_not be_valid
+    end
+
+    it "name attribute is too long" do
+      city = FactoryBot.build(:city, name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed sem lectus.
+      Ut non fermentum velit. Nunc facilisis diam id ante auctor rutrum. Proin mauris ipsum, scelerisque et convallis
+      ac, molestie quis turpis. Vivamus rhoncus ex non arcu mattis, non sollicitudin est pellentesque. Curabitur neque
+      risus, dignissim placerat enim eget, facilisis efficitur nisl. In dictum, diam ullamcorper hendrerit malesuada,
+      augue magna cursus sapien, dignissim interdum elit ante non dui. Suspendisse tempus orci vel orci imperdiet,
+      eget rutrum lacus cursus. In dictum facilisis mauris, id euismod neque tincidunt aliquet. Aenean consectetur
+      lacus eget efficitur sagittis. Phasellus a sem eu leo viverra pharetra vitae in metus. Fusce suscipit scelerisque
+      faucibus. Proin et ex porta, suscipit erat et, semper enim. In lobortis egestas nulla dictum aliquet. Sed eget
+      consectetur nibh. Maecenas egestas et quam scelerisque consequat.")
+
+      expect(city).to_not be_valid
+    end
+
+    it "name attribute is too short" do
+      city = FactoryBot.build(:city, name: "")
+
+      expect(city).to_not be_valid
+    end
+  end
+
+  it "It can have many reservation records" do
+
+    engine_type = FactoryBot.create(:engine_type)
+    car = FactoryBot.create(:car)
+    car_detail = FactoryBot.create(:car_detail, engine_type: engine_type, car: car)
+
+    reservation1 = FactoryBot.build(:reservation, car: car, city: city)
+    reservation2 = FactoryBot.build(:reservation, car: car, city: city)
+
+    city.reservations << reservation1
+    city.reservations << reservation2
+
+    expect(city.reservations).to include(reservation1, reservation2)
+  end
 end
