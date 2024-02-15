a6cc58941616d622761cf1ca4f5de4dca7ce085d
Esteban <cepalacios@outlook.com>
2024-02-15 17:13:44 -0500

    Refactor city model with more validations

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ApplicationRecord
   has_many :reservations, dependent: :destroy
 
-  validates :name, presence: true, type: String, length: { minimum: 1 }
+  validates :name, presence: true, length: { minimum: 1 }
 end
