f6426bbc8048a4bf43f3184add45904405ad8298
Esteban <cepalacios@outlook.com>
2024-02-15 17:03:17 -0500

    Refactor car model so it has one to one association with car_detail and so it accepts nested attributes from car_detail

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,8 @@
 class Car < ApplicationRecord
   has_many :reservations, dependent: :destroy
-  has_many :car_details, dependent: :destroy
+  has_one :car_detail, dependent: :destroy
+
+  accepts_nested_attributes_for :car_detail
 
   validates :name, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 10 }
