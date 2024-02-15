f0c88fb27adae8fae367307d9bf65a141d213e9e
Esteban <cepalacios@outlook.com>
2024-02-15 17:06:24 -0500

    Refactor car_details model with more validations

--- app/models/car_detail.rb
+++ app/models/car_detail.rb
@@ -1,5 +1,7 @@
 class CarDetail < ApplicationRecord
   belongs_to :car
   belongs_to :engine_type
-  validates :car_id, :engine_type_id, presence: true
+  validates :horsepower, :torque, numericality: { only_integer: true, greater_than: 0 }
+  validates :fuel_economy, :seating_capacity, :cargo_space, :infotainment_system, :safety_rating, :tech_features, :special_features, presence: true
+  validates :fuel_economy, :cargo_space, length: { maximum: 255 }
 end
