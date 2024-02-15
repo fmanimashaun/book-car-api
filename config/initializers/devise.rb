c30c10b8e597e01d464f7d2fa0b78371daac2c06
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-15 20:36:06 +0100

    Update github action

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -312,7 +312,7 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
     jwt.dispatch_requests = [
       ['POST', %r{^/login$}]
     ]
