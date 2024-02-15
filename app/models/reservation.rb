e451ede95fb471961449946a5831abe1b7708ebe
Esteban <cepalacios@outlook.com>
2024-02-15 17:12:34 -0500

    Refactor reservation model with more validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,4 +4,9 @@ class Reservation < ApplicationRecord
   belongs_to :user
 
   validates :date, :car_id, :city_id, :user_id, presence: true
+  validate :date_cannot_be_in_the_past
+
+  def date_cannot_be_in_the_past
+    errors.add(:date, "can't be in the past") if date.present? && date < Date.today
+  end
 end
