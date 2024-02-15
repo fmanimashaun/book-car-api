ea9674139224f97cc0f2f340db89ad9e19c1b18e
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-02-16 01:42:26 +0430

    Create validation to the car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,7 @@
 class Car < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :car_details, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :description, presence: true, length: { minimum: 10 }
 end
