23592821d7f73d35adc690dfcf22d7deee5e8b6c
Esteban <cepalacios@outlook.com>
2024-02-15 17:18:37 -0500

    Add maximum length to city model

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ApplicationRecord
   has_many :reservations, dependent: :destroy
 
-  validates :name, presence: true, length: { minimum: 1 }
+  validates :name, presence: true, length: { minimum: 1, maximum: 255 }
 end
