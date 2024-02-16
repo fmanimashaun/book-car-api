8a35d43d731d619b52fd3941b1ed1ecf37f9d225
Esteban <cepalacios@outlook.com>
2024-02-16 10:29:41 -0500

    Add tests for car_detail_rspec

--- spec/models/car_detail_spec.rb
+++ spec/models/car_detail_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe CarDetail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:car) { FactoryBot.create(:car) }
+  let(:engine_type) { FactoryBot.create(:engine_type) }
+
+  let(:car_detail) { FactoryBot.create(:car_detail, car: car, engine_type: engine_type) }
+
+  it "Creates a reservation with valid attributes" do
+    expect(car_detail).to be_valid
+  end
+
+  it "is invalid withinvalid attributes" do
+    car_detail = FactoryBot.build(
+      :car_detail,
+      horsepower: nil,
+      car: nil,
+      torque: nil,
+      fuel_economy: nil,
+      seating_capacity: nil,
+      seating_capacity: nil,
+      cargo_space: nil,
+      infotainment_system: nil,
+      safety_rating: nil,
+      tech_features: nil,
+      special_features: nil
+    )
+
+    expect(car_detail).to_not be_valid
+  end
 end
