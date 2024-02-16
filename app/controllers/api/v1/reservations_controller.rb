14da7b047788870e40a68c5a98e3502010494f8c
Anwar Hussaini <anwarhussaini160@gmail.com>
2024-02-16 20:55:03 +0430

    Create the private helper functions and call them befor action

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,4 +1,12 @@
 class API::V1::ReservationsController < ApplicationController
+  before_action :set_reservation, only: [:destroy, :show, :update]
+  private
 
+  def set_reservation
+    @reservation = Reservation.find params[:id]
+  end
 
+  def reservation_params
+    params.require(:reservation).permit(:date, :city_id, :car_id, user_id)
+  end
 end
