c923371d0adfa2a156956b4474a52df5981b765e
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-17 10:07:43 +0100

    Revert "Implement Car CRUD Operations"

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :cities
-      resources :cars, except: [:index]
-      resources :engine_type, except: [:index]
     end
   end
-
+  
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   get '/current_user', to: 'users/current_user#index'
