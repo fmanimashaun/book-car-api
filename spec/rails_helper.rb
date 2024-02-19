4c8c468f15746a6f376f11a3770fa3b5e46d82a4
Esteban <cepalacios@outlook.com>
2024-02-19 14:28:46 -0500

    Add devise test configurations to rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,21 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+   # Configuration for using devise in testing for controllers
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  # Configuration for using devise in testing for requests
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  # Configuration for using devise in testing for views
+  config.include Devise::Test::ControllerHelpers, type: :view
+
+  # Facilitates user authentication in feature specs
+  config.include Warden::Test::Helpers, type: :feature
+
+  # configurations for Shoulda Matchers
+  config.include FactoryBot::Syntax::Methods
+  config.include Shoulda::Matchers::ActiveModel, type: :model
+  config.include Shoulda::Matchers::ActiveRecord, type: :model
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
