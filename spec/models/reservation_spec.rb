be193797280578bb3c5f6514a0f3f262600d0b10
Esteban <cepalacios@outlook.com>
2024-02-19 21:02:11 -0500

    Refactor reservation model spec so the date in past has more than one day difference

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+require 'pry'
+
 RSpec.describe Reservation, type: :model do
   let(:reservation) { build(:reservation) }
 
@@ -10,9 +12,10 @@ RSpec.describe Reservation, type: :model do
   end
 
   it 'validates date cannot be in the past' do
-    reservation.date = Date.yesterday
-    expect(reservation).not_to be_valid
-    expect(reservation.errors[:date]).to include("can't be in the past")
+    reservation = build(:reservation, date: (Date.yesterday - 2))
+
+  expect(reservation).not_to be_valid
+  expect(reservation.errors[:date]).to include("can't be in the past")
   end
 
   it 'belongs to a city' do
