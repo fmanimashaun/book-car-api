52c1512a74299ac0eb0a780c7ffb1981c7ae1f2c
Esteban <cepalacios@outlook.com>
2024-02-23 22:16:44 -0500

    Modify the set_image_url from car model

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.action_controller.default_url_options = { host: "dpg-cncleomn7f5s73bhduug-a" }
+
 end
