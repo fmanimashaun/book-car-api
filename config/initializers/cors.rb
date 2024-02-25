bf3795e9adebb9e4bfe9a7497f7c13483e8a3717
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-02-25 12:54:15 +0100

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://fmanimashaun.github.io/book-car/'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
