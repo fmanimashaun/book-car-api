3dde9e34a34045fa94ed91b47c2247a45a93169c
Esteban <cepalacios@outlook.com>
2024-02-26 11:47:49 -0500

    Update production environment to have the database as host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_controller.default_url_options = { host: "book-car-api.onrender.com" }
 end
+Rails.application.routes.default_url_options[:host] = 'book-car-api.onrender.com'
