d4cee1e6bca4478672dfbdd233678e93d8967456
Esteban <cepalacios@outlook.com>
2024-02-26 12:52:51 -0500

    Update databse url in production environment and swagger.yaml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.action_controller.default_url_options = { host: "book-car-api.onrender.com" }
+  config.action_controller.default_url_options = { host: "https://book-car-api-rygf.onrender.com" }
 end
 
-Rails.application.routes.default_url_options[:host] = 'book-car-api.onrender.com'
+Rails.application.routes.default_url_options[:host] = 'https://book-car-api-rygf.onrender.com'
